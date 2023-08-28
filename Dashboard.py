559f2e6e33142f8521ca2b8aafa8747c9a3405a7
JulianJar <120107149+JulianJar@users.noreply.github.com>
2023-08-28 12:10:45 +0200

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -16,7 +16,7 @@ if fl is not None:
     st.write(filename)
     df = pd.read_csv(filename, encoding = "ISO-8859-1")
 else:
-    os.chdir(r"https://github.com/JulianJar/eComExample/")
+    os.chdir(r"https://github.com/JulianJar/eComExample/tree/main")
     df = pd.read_excel("Superstore.xls")
 
 col1, col2 = st.columns((2))
